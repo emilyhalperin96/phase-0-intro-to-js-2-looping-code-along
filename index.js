08082bd623746a31495511947c5e740fd38087ee
emilyphalperin@gmail.com <emilyhalperin@Emilys-MacBook-Pro-2.local>
2023-01-11 16:55:53 -0500

    done

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+  const messages = []
+  for (let i = 0; i < names.length; i++) {
+    const str = `Thank you, ${names[i]}, for the wonderful ${event} gift!`
+    messages.push(str)
+  }
+  return messages
+}
+console.log(writeCards(['emily', 'john'], 'birthday'))
+
+
+function countDown(num) {
+  while (num >= 0) {
+    console.log(num)
+    num--
+  }
+}
